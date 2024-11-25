97591e8ac48aeac129f0b08b82cfce682392ac11
awatkin <144458571+awatkin@users.noreply.github.com>
2024-11-25 13:34:57 +0000

    Added code and task for nov 24.

---
+++ Real Assessments/ESP Nov 2024/Prep Task/VersereCars.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+# Outputs the initial menu and checks validates the input
+def main_menu():
+    flag = True
+
+    while flag:
+
+        print("####################################################")
+        print("##############   Versere Cars Menu    ##############")
+        print("####################################################")
+        print("")
+        print("########### Please select an option ################")
+        print("### 1. Total sales income from different models")
+
+        choice = input('Enter your number selection here: ')
+
+        try:
+            int(choice)
+        except:
+            print("Sorry, you did not enter a valid option")
+            flag = True
+        else:
+            print('Choice accepted!')
+            flag = False
+
+    return choice
+
+
+# Submenu for totals, provides type check validation for the input
+def total_menu():
+    flag = True
+
+    while flag:
+
+        print("####################################################")
+        print("############## Total income by Model ##############")
+        print("####################################################")
+        print("")
+        print("########## Please select an income source ##########")
+        print("### 1. Ranger")
+        print("### 2. Model D Premium Plus")
+        print("### 3. Compass")
+        print("### 4. Mercury")
+        print("### 5. Ranger")
+        print("### 6. Outback")
+
+        choice = input('Enter your number selction here: ')
+
+        try:
+            int(choice)
+        except:
+            print("Sorry, you did not enter a valid option")
+            flag = True
+        else:
+            print('Choice accepted!')
+            flag = False
+
+    return choice
+
+
+# takes the total submenu input and converts the number to a string of the source name
+def convert_total_men_choice(total_men_choice):
+    if total_men_choice == "1":
+        tot_choice = "Ranger"
+    elif total_men_choice == "2":
+        tot_choice = "Model D Premium Plus"
+    elif total_men_choice == "3":
+        tot_choice = "Compass"
+    elif total_men_choice == "4":
+        tot_choice = "Mercury"
+    elif total_men_choice == "5":
+        tot_choice = "Ranger"
+    else:
+        tot_choice = "Outback"
+
+    return tot_choice
+
+
+# creates a new dataframe with the selected income source then creates a total row
+# outputs the final total in a message
+def get_total_data(total_choice):
+    df = pd.read_csv("Task3_data.csv")
+
+    income = df[df["Car Model"]==total_choice]
+    total = income[["Value"]].sum().sum()
+
+    msg = "The total income from {} was: £{}".format(total_choice, total)
+    return msg
+
+
+main_menu_choice = main_menu()
+if main_menu_choice == "1":
+    total_men_choice = total_menu()
+    total_choice = convert_total_men_choice(total_men_choice)
+    print(get_total_data(total_choice))
---
+++ Real Assessments/ESP Nov 2024/README.md
@@ -0,0 +1,45 @@
+# Pandas and Matplotlib NOV 2024
+
+Sample code to branch for students to test ahead of the exam
+
+The datafile as been simulated to what you could see in the exam, the core code base is similar (as possible) to what you will have in the exam
+
+This is a teaching tool to be able to get your head around Pandas and matplotlib
+
+FOLDERS:
+    
+    MAIN: This is the starting point for you to take and then work from
+
+    Sol1: Gives the solution to problem 1 below
+    Sol2: Gives the solution to problem 1 and 2 below
+    Sol3: Gives the solution to problem 1,2 and 3 below
+    Sol4: Gives the solution to problem 1,2,3 and 4 below
+    SolFinal: Gives the soltion to all problems and is robustness and well commented
+
+
+
+## Task:
+
+These are:
+
+Problem 1: Show the Sales over time for a specific sales peron
+Problem 1a: Show the total sales for each of the sales people
+
+Problem 2: Show the total sales for each of the Brands
+Problem 2a: Show the total sales for each of the Car Models
+
+Problem 3: Compare the total sales for more than 1 brand
+Problem 3a: Compare the total sales for more than 1 model
+
+Problem 4: Show the sales for a Specific Brand over a period of time
+Problem 4a: Show the sales for a Specific Model over a period of time. 
+
+
+The solution must handle data in a secure way.
+
+They have also provided user requirements for the solution. These are: 
+- Program must be easy to use
+- Display all information in a meaningful way
+- Make use of appropriate textual, numerical and graphical output in a way that would be relevant to the end user. 
+
+Note – the data shows the number of each menu item sold for each of the given days.
