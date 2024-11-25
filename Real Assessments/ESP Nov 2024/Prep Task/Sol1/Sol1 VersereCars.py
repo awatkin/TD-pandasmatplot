2ec82e2f372cb90bd8f0b5fa3096fc7a527d68e6
awatkin <144458571+awatkin@users.noreply.github.com>
2024-11-25 16:44:55 +0000

    Completed problem 1 and 1a from task sheet.

--- Real Assessments/ESP Nov 2024/Prep Task/Sol1/Sol1 VersereCars.py
+++ Real Assessments/ESP Nov 2024/Prep Task/Sol1/Sol1 VersereCars.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt  # added new import to bring graphing library in.
+
 
 # Outputs the initial menu and checks validates the input
 def main_menu():
@@ -12,8 +14,8 @@ def main_menu():
         print("")
         print("########### Please select an option ################")
         print("### 1. Total sales income from different models")
-        print("### 2. Sales over time for a specific sales person")  # New menu option added for task 1.
-
+        print("### 2. Problem 1 Sales over time for a specific sales person")  # New menu option added for task 1.
+        print("### 3. Problem 1a Total sales for each salesperson")  # New menu option added for task 1a.
         choice = input('Enter your number selection here: ')
 
         try:
@@ -78,6 +80,76 @@ def convert_total_men_choice(total_men_choice):
     return tot_choice
 
 
+# new subroutine for sales peron over time problem 1
+def sales_person_overtime(sales_person_c):
+    df = pd.read_csv("Task3_data.csv")  # reads in the dataframe (fresh and clean)
+    if sales_person_c == "1":  # conditional statement to select the sales person, to stop input error
+        spc = "Christopher Clark"
+    elif sales_person_c == "2":
+        spc = "Michael Garcia"
+    elif sales_person_c == "3":
+        spc = "David Johnson"
+    elif sales_person_c == "4":
+        spc = "Sarah Jones"
+
+    df1 = df[(df['Salesperson']==spc)]  # slices the df to only show our selected salesperson
+    print("The sales over time for ", spc, " :")  # nice message before printing out.
+    print(df1)  # print out the data for chosen sales person over time, problem 1 complete.
+    print("")
+    print("")
+    graphdf = df1[['Date','Value']]  # strips dataset down to just data and value.
+    graphdf.set_index('Date', inplace=True)  # sets the date to be index so that dates show on graph with sales for each date.
+    title = spc, ": Individual sales totals"  # concat for title of chart
+    graphdf.plot.bar()  # plot figures as a bar chart
+    plt.title(title)  # set title
+    plt.show()  # show the graph
+    print("The total sales for ", spc, " are:")
+    df1 = df1['Value'].sum()  # calculate total sales for that sales person.
+    print(df1)
+
+
+# new subroutine to work out which of the sales people you want to view.
+def sales_person_menu():
+    flag = True  # sets a check flag to help with input validation
+
+    while flag:  # starts the while loop with the flag
+
+        print("####################################################")
+        print("############## Sales Person Selector  ##############")
+        print("####################################################")
+        print("")
+        print("########## Please select an income source ##########")
+        print("### 1. Christopher Clark")
+        print("### 2. Michael Garcia")
+        print("### 3. David Johnson")
+        print("### 4. Sarah Jones")
+
+        choice = input('Enter your number selction here: ')  # Captures user input of number
+
+        try:
+            int(choice)  # try to int it, ensure its a number
+        except:
+            print("Sorry, you did not enter a valid option")  # if not, error messages
+            flag = True  # set flag to loop again
+        else:
+            print('Choice accepted!')  # accept and move on
+            flag = False
+
+    return choice  # return the number they chose when correct.
+
+
+# new subroutine to solve problem 1a, total for each sales person
+def salesperson_indi_totals():  # declartion of the new subroutine
+    df = pd.read_csv("Task3_data.csv")  # Read in fresh clean dataframe
+    df1 = df[['Value','Salesperson']]  # slices away unneeded columns
+    df1 = df1.groupby('Salesperson').sum()  # sums the data for each salesperson
+    print("The total sales for each individual salesperson is :")
+    print(df1)  # prints out the data for the user.
+    df1.plot.bar()
+    plt.title("Sales Person Individual sales totals")
+    plt.show()
+
+
 # creates a new dataframe with the selected income source then creates a total row
 # outputs the final total in a message
 def get_total_data(total_choice):
@@ -95,5 +167,9 @@ if main_menu_choice == "1":
     total_men_choice = total_menu()
     total_choice = convert_total_men_choice(total_men_choice)
     print(get_total_data(total_choice))
-elif main_menu_choice == "2":
-    pass
+elif main_menu_choice == "2":  # new condition on selection statement for Problem 1.
+    sales_person_choice = sales_person_menu()  # calls a subroutine to find the sales person
+    sales_person_overtime(sales_person_choice)
+elif main_menu_choice == "3":  # new condition on selection statement for Problem 1.
+    salesperson_indi_totals()
+
