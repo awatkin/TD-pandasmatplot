3862708136c93ac05af886cce81bba99670236fc
adam <adam.watkin@utcleeds.co.uk>
2025-01-16 07:37:23 +0000

    Partially completed the time of day performance, just mainpulate the data frame based on user input to do.

--- Real Assessments/WIP Nov 2023/Official Files/WIP Solution/Task4a_AnimalRescue.py
+++ Real Assessments/WIP Nov 2023/Official Files/WIP Solution/Task4a_AnimalRescue.py
@@ -6,15 +6,42 @@ def dfreadin():  #added a subroutine so DF is only created in one place for main
     return df
 
 def makegraph(df,graph_type, title, x_axis, y_axis):  # new subroutine to call to make a graph
-    if graph_type == "bar":
-        df.plot.bar()
+    if graph_type == "bar":  # checks if it should be a bar chart
+        df.plot.bar()  # plots as a bar chart if true
     else:
-        df.plot()
-    plt.title(title)
-    plt.xlabel(x_axis)
-    plt.ylabel(y_axis)
-    plt.show()
+        df.plot()  # if not, builds a normal graph
+    plt.title(title)  # Sets the title of the plot based on user input
+    plt.xlabel(x_axis)  # sets the x axis from the parameter
+    plt.ylabel(y_axis)  # Sets the y axis label based on the user set parammeter
+    plt.show()  # shows the graph
 
+def post_type_select():
+    flag = True
+
+    while flag:
+
+        print("#################################################")
+        print("############ Time of Day Interaction ############")
+        print("#################################################")
+        print("")
+        print("########### Please select an option #############")
+        print("### 1. Likes Performmance")
+        print("### 2. Shares Performance")
+        print("### 3. Comments Performance")
+        print("### 4. All interactions Performance")
+
+        choice = input('Enter your number selction here: ')
+
+        try:
+            int(choice)
+        except:
+            print("Sorry, you did not enter a valid option")
+            flag = True
+        else:
+            print('Choice accepted!')
+            flag = False
+
+    return choice
 
 def main_menu():
     flag = True
@@ -27,8 +54,8 @@ def main_menu():
         print("")
         print("########### Please select an option #############")
         print("### 1. Average Social Media Interaction Data")
-        print("### 2. Popularity of each post type")
-        print("### 3. Time of day performance for posts")
+        print("### 2. Popularity of each post type")  # added new option to allow looking at popularity of each type of post
+        print("### 3. Time of day performance for posts")  # Added a new option to search for performance at times of day
         print("")
         print("### 7. Quit")
 
@@ -122,7 +149,15 @@ def post_popularity():  # new subroutine to total the interactions for each post
     print("")
     print("Here is the total interactions for each post type")  # friendly way to output
     print(extract)  # prints out the results
-    makegraph(extract,"bar", "Total Interactions per type","Interaction type", "Total Interations")
+    return extract,"bar", "Total Interactions per type","Interaction type", "Total Interations"
+
+
+def time_of_day_performance(type_to_use):  # new sub routine to look at performance through the day
+    graph_type = ""  # sets variable to collect graphs type
+    title = ""  # sets variable to collect graph title
+    x_axis = ""  # variable to collect X axis label
+    y_axis = ""  # variable to collect the y axis label
+    df = dfreadin()  # collects a fresh dataframee to maniputlate
 
 
 def main():
@@ -133,7 +168,12 @@ def main():
             avg_choice = convert_avg_men_coice(avg_men_choice)
             print(get_avg_data(avg_choice))
         elif main_menu_choice == "2":  # added a new menu option for the popularity of each typee of post
-            post_popularity()
+            df, graph_type, title, x_axis, y_axis = post_popularity()
+            makegraph(df, graph_type, title, x_axis, y_axis)
+        elif main_menu_choice == "3":
+            type_to_use = post_type_select()
+            df, graph_type, title, x_axis, y_axis = time_of_day_performance(type_to_use)
+            makegraph(df, graph_type, title, x_axis, y_axis)
         elif main_menu_choice == "7":
             print("Thanks for using our system, Goodbye")
             quit()
